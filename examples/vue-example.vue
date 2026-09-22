6f7eb02db24d51870bf691a02e602f8e3a43769a
Mig Tam Rod <migtam628@gmail.com>
2026-09-22 13:21:16 -0400

    docs: add examples/vue-example.vue

---
+++ examples/vue-example.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { MoviePlayer } from 'vidplus-movie-player/vue';
+</script>
+
+<template>
+  <main style="max-width: 960px; margin: 2rem auto; padding: 0 1rem">
+    <MoviePlayer
+      type="movie"
+      :id="27205"
+      primarycolor="00D4FF"
+      :autoplay="false"
+      title-attr="VidPlus movie player"
+    />
+  </main>
+</template>
