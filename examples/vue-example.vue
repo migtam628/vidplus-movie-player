9fa63f5a640c227d48d53fe3f37b5affb15d6db4
Mig Tam Rod <migtam628@gmail.com>
2026-09-22 13:37:33 -0400

    docs: update Vue example for VidUp

--- examples/vue-example.vue
+++ examples/vue-example.vue
@@ -6,10 +6,12 @@ import { MoviePlayer } from 'vidplus-movie-player/vue';
   <main style="max-width: 960px; margin: 2rem auto; padding: 0 1rem">
     <MoviePlayer
       type="movie"
-      :id="27205"
-      primarycolor="00D4FF"
-      :autoplay="false"
-      title-attr="VidPlus movie player"
+      id="tt0480249"
+      :auto-play="false"
+      theme="00D4FF"
+      sub="en"
+      :chromecast="false"
+      title-attr="VidUp movie player"
     />
   </main>
 </template>
